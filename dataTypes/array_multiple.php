5444a3462a8ec6725865f87415cfce51193f93ab
YtuUzem <YtuUzem@HasanTLaptop>
2014-01-25 11:45:13 +0200

    -array ornekgi yapıldı

---
+++ dataTypes/array_multiple.php
@@ -0,0 +1,25 @@
+<?php
+$musteri1 = array(
+    'ad'=>'Hasan',
+    'soyad'=>'Tokatlı',
+    'email'=>'cehasan@gmail.com',
+    'adres'=>array(
+        'ev'=>'Eyüp',
+        'fatur'=>'Beşiktaş'
+    ),
+    'banka'=>array(
+        array(
+            'bankaadi'=>'Garanti Bank',
+            'subekod'=>6523,
+            'hesapno'=>61546546,
+            'ekbilgi'=>'maas hesabı'
+        ),
+        array(
+            'bankaadi'=>'İş Bankası',
+            'subekod'=>9854,
+            'hesapno'=>332315,
+            'ekbilgi'=>'kişisel hesabı'
+        )        
+    )
+);
+
